b205573e0234107488035b377b08d0c9d4ce2fa3
Hansa Jayathilaka <hansajayathilaka@yahoo.com>
2020-11-28 21:39:33 +0530

    Remove Stages architechere

--- Automation_Temparature_System.ino
+++ Automation_Temparature_System.ino
@@ -5,7 +5,7 @@
 #define I2C_ADDR 0x3F //I2C adress, you should use the code to scan the adress first (0x27) here
 //#define I2C_ADDR 0x27
 #define BACKLIGHT_PIN 3 // Declaring LCD Pins
-#define En_pin 2
+#define En_pin 8
 #define Rw_pin 1
 #define Rs_pin 0
 #define D4_pin 4
@@ -22,60 +22,78 @@
 #define ir_pin A7
 
 const int proximity_max_distance = 170;
-const int wait_time_for_IR_sensor = 100;
+const int wait_time_for_IR_sensor = 500;
 const int max_temparature = 37;
 
-bool std_state = false;
+char std_state = 'N'; // G, B, N
 
 LiquidCrystal_I2C lcd(I2C_ADDR, En_pin, Rw_pin, Rs_pin, D4_pin, D5_pin, D6_pin, D7_pin);
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
+void high_temp_door_interupt();
+void low_temp_door_interupt();
 
 int Stage = 0;
+
 
 void setup() {
   Serial.begin(9600);
 
   mlx.begin();
-  lcd.begin (16, 2);
+  lcd.begin (20, 4);
   lcd.setBacklightPin(BACKLIGHT_PIN, POSITIVE);
   lcd.setBacklight(HIGH); //Lighting backlight
   lcd.home ();
 
   pinMode(ir_pin, INPUT);
-  Stage = 1;
+
+  attachInterrupt(digitalPinToInterrupt(low_temp_door_pin), low_temp_door_interupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(high_temp_door_pin), high_temp_door_interupt, FALLING);
+
 }
 
-
 void loop() {
-  //  const int temparature = temp();
-  //  const int len = len_to_hand();
-  //  Serial.print("Temparature : ");
-  //  Serial.println(temparature);
-  //  Serial.print("Length      : ");
-  //  Serial.println(len);
-  //  Serial.println();
-  //  delay(200);
 
-  //    lcd.setCursor(0, 1);
-  //    lcd.print("Target  ");
-  //    lcd.print(mlx.readObjectTempC());
-  //    lcd.print(" C");
+  const int pre_time = millis();
+  while (len_to_hand() > proximity_max_distance) {
+    if (millis() - pre_time > wait_time_for_IR_sensor) {
 
-  switch (Stage) {
-    case 1:
-      Serial.println("Stage 1");
-      stage_1();
-      break;
-    case 2:
-      Serial.println("Stage 2");
-      stage_2();
-      break;
-    case 3:
-      Serial.println("Stage 3");
-      stage_3();
-      break;
-    default:
-      break;
+
+
+
+
+      float temparature = 0;
+      const int count = 10;
+      for (int i = 0; i < count; i++) {
+        temparature += temp();
+      }
+      temparature /= count;
+
+      lcd.clear();
+      lcd.setCursor(1, 0);
+      lcd.print("Your Temparature");
+      lcd.setCursor(6, 1);
+      lcd.print(temparature);
+      lcd.print(" C");
+      lcd.setCursor(3, 2);
+      lcd.print("Thank You");
+      lcd.setCursor(6, 3);
+      lcd.print("Stay Safe");
+      Serial.println(temparature);
+
+      if (temparature < max_temparature) {
+        std_state = 'G';
+        low_temp_door_signal_on();
+      } else  {
+        std_state = 'B';
+        high_temp_door_signal_on();
+      }
+
+
+
+
+
+    }
   }
+
 }
--- Services.ino
+++ Services.ino
@@ -4,26 +4,9 @@ float temp() {
   //  Serial.print("Target  ");
   //  Serial.println(mlx.readObjectTempC());
   //  Serial.println();
-  const int x = 27, y = 35
-                        , z = 0;
-  const float val = mlx.readObjectTempC() / x * y + z;
+  const int x = 1, y = 0;
+  const float val = mlx.readObjectTempC() * x + y;
   return val;
-}
-
-int len_to_hand() {
-  const int val = analogRead(ir_pin);
-  //  Serial.println(val);
-  return val;
-}
-
-void check_for_hand_distance() {
-  const int pre_time = millis();
-  while (len_to_hand() > proximity_max_distance) {
-    if (millis() - pre_time > wait_time_for_IR_sensor) {
-      Stage++;
-      return;
-    }
-  }
 }
 
 void low_temp_door_signal_on() {
@@ -36,95 +19,48 @@ void high_temp_door_signal_on() {
   digitalWrite(high_temp_led_pin, HIGH);
 }
 
+void door_signal_initialization() {
+  digitalWrite(low_temp_led_pin, LOW);
+  digitalWrite(high_temp_led_pin, LOW);
+}
+
 void wrong_door_alarm() {
 }
 
 void low_temp_door_interupt() {
   Serial.println("Low temparature intrupted fired");
-  Serial.println(Stage);
   Serial.println(std_state);
-
-  lcd.clear();
-  lcd.setCursor(1, 0);
-  lcd.print("Low Door");
-
-  if (Stage == 4) {
-    if (std_state == true) {
-      Stage = 1;
+  switch (std_state) {
+    case 'N':
+      door_signal_initialization();
+      break;
+    case 'G':
+      std_state = 'N';
       lcd.clear();
-    } else {
+      lcd.setCursor(6, 0);
+      lcd.print("Wellcome");
+      break;
+    case 'B':
       wrong_door_alarm();
-    }
+      break;
   }
 }
 
 void high_temp_door_interupt() {
   Serial.println("High temparature intrupted fired");
-  Serial.println(Stage);
   Serial.println(std_state);
-
-  lcd.clear();
-  lcd.setCursor(1, 0);
-  lcd.print("High Door");
-
-  if (Stage == 4) {
-    if (std_state == true) {
+  switch (std_state) {
+    case 'N':
+      door_signal_initialization();
+      break;
+    case 'G':
       wrong_door_alarm();
-    } else {
-      Stage = 1;
+      break;
+    case 'B':
+      std_state = 'N';
       lcd.clear();
-    }
+      lcd.setCursor(6, 0);
+      lcd.print("Wellcome");
+      break;
   }
-}
-
-
-void stage_1() {
-  digitalWrite(low_temp_led_pin, LOW);
-  digitalWrite(high_temp_led_pin, LOW);
-
-  lcd.setCursor(6, 0);
-  lcd.print("Wellcome");
-  //  lcd.setCursor(1, 1);
-  //  lcd.print("Pull your hand near to the temparature sensor");
-
-  check_for_hand_distance();
-}
-
-
-void stage_2() {
-  float temparature = 0;
-  const int count = 50;
-  for (int i = 0; i < count; i++) {
-    temparature += temp();
-  }
-  temparature /= count;
-
-  lcd.clear();
-  lcd.setCursor(1, 0);
-  lcd.print("Your Temparature");
-  lcd.setCursor(6, 1);
-  lcd.print(temparature);
-  lcd.print(" C");
-
-  std_state = temparature < max_temparature;
-
-  if (std_state) {
-    low_temp_door_signal_on();
-  } else  {
-    high_temp_door_signal_on();
-  }
-  Stage++;
-  delay(3000);
-  lcd.clear();
-}
-
-
-void stage_3() {
-  attachInterrupt(digitalPinToInterrupt(low_temp_door_pin), low_temp_door_interupt, FALLING);
-  attachInterrupt(digitalPinToInterrupt(high_temp_door_pin), high_temp_door_interupt, FALLING);
-  lcd.setCursor(4, 0);
-  lcd.print("Thank You");
-  lcd.setCursor(5, 1);
-  lcd.print("Stay safe");
-  Stage++;
 }
